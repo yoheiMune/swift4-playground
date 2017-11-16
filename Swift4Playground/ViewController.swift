79cdfe5fc8a20af3eaa5f73f3c5c0a635e60007b
Yohei Munesada <y.munesada@gmail.com>
2017-11-16 09:56:29 +0900

    Add an empty app.

---
+++ Swift4Playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4Playground
+//
+//  Created by Munesada Yohei on 2017/11/16.
+//  Copyright © 2017年 Munesada Yohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
